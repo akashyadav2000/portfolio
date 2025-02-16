a66aee2cdc12df5a782157554e535fe43ea5e0d1
akashyadav2000 <125122328+akashyadav2000@users.noreply.github.com>
2025-02-16 22:52:33 +0530

    m

--- src/Components/Data/project.js
+++ src/Components/Data/project.js
@@ -25,7 +25,7 @@ const Data = [
     description:
       "Lodha Amara is a sleek and engaging real estate web application showcasing premium residences. Built for seamless user experience and effortless property exploration.",
     image: "/Project-Images/lodha_amara.webp",
-    link: "https://todo-lists-web-app.vercel.app/",
+    link: "lodha-amara-clone.vercel.app/",
   },
 
   {
